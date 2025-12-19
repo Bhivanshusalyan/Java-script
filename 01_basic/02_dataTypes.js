922f16689d2a3919a29fe4d24cc64a2a570f13bf
Bhivanshu salyan <bhivanshusalyan123@gmail.com>
2025-12-19 08:40:10 +0000

    data types done

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,17 @@
+" use strict" // use JS as a new version
+
+// alert( 3+3 ) We are using nodeJS , not browser.
+
+// number => 2 to power 53
+// bigint in stock market
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique    
+
+
+console.log(typeof null); // object
+console.log((typeof undefined)); // undefine
+
+
