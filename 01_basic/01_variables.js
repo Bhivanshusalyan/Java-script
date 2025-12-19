2c6b34914541c8c2917e5e503a6e0f927b49a6d5
Bhivanshu salyan <bhivanshusalyan123@gmail.com>
2025-12-19 08:38:33 +0000

    variable config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = "234"
+let accountEmail = "bhi@1.com"
+var accountPassword = "0986" 
+accountCity = "kanpur"
+let accountState;
+
+/*
+   var variable is not allowed in js we use let.
+*/
+
+
+accountEmail = "bhivaan1@gmail.com"
+accountPassword = "jaat"
+accountCity = "bijnor"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
