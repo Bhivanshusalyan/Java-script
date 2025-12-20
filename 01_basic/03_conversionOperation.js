e26442e70459a7d5c34575c2d6c2f3cf65f03d38
Bhivanshu salyan <bhivanshusalyan123@gmail.com>
2025-12-20 07:43:05 +0000

    done

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -13,12 +13,54 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn );
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn );
 
 // 1=> true
 // "" => false
 // "bhivaan" => true
+
+
+
+// **********************************Operations************************************
+
+// let value = 5
+// let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello"
+// let str2 = " bhivaan"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
+
+
+
+
+
 
 
 
