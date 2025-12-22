cf471ed7735156fc49bb5891de560f6b1c0c9f40
Bhivanshu salyan <bhivanshusalyan123@gmail.com>
2025-12-22 11:25:03 +0000

    memory

---
+++ 01_basic/06_string.js
@@ -0,0 +1,42 @@
+const name = "Bhivaan"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Bhivanshu-salyan-jaat')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 5)
+console.log(anotherString);
+
+const newStringOne = "      Bhivaan     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://bhivan.com/bhivan%20salyan "
+
+console.log(url.replace('%20', '-'))
+console.log(url.includes('bhivan'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
