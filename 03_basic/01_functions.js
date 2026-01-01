f1a9c517f0e05859fe75d66394663ca7d6c29223
Bhivanshu salyan <bhivanshusalyan123@gmail.com>
2026-01-01 18:37:10 +0000

    functions

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -29,12 +29,49 @@ const result = addTwoNumbers(3, 4)
 
 
 function loginUserMessage(username = "sam"){
-    // if(username === undefined){                        if(!username)
+    if(username === undefined){                        if(!username)
         console.log("please enter a username");
         return
     }
-    return `${username} just logged in`
+    return`${username} just logged in`
 }
 
-// console.log(loginUserMessage("hitesh"))
-console.log(loginUserMessage())
+    // console.log(loginUserMessage("bhivaan"));
+    // console.log(loginUserMessage());
+
+
+function calculateCartPrise(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrise(200, 400, 500));
+
+const user = {
+    username: "bhivaan",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 300, 500, 800]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 800]));
+
+
+
+    
+    
