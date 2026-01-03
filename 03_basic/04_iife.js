9fa3ff21b58b5d2e449ee8a69eb3d5ac7f03f2e5
Bhivanshu salyan <bhivanshusalyan123@gmail.com>
2026-01-03 06:50:26 +0000

    iife

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,10 @@
+// Immediately Invoked Function Expression (IIFT)
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )("bhivanshu")
